095cea8e9a8dea46177836e848c782c468befae1
IYP-Programer-Yeah <hgsilvermann@yahoo.com>
2019-04-19 21:40:30 +0430

    More testing :|

--- codecov.sh
+++ codecov.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for filename in `find . | egrep '\.cpp'`; 
-do 
-  gcov-5 -n -o . $filename > /dev/null; 
+for filename in `find . | egrep '\.gcno'`;
+do
+  sed -i 's/\/Projects\/CMake\/Build\/include\/MDP/\/Messenger\/Include/g' $filename;
 done
